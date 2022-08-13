1663c23c9b15e80a0e44209bf7f9fd263f4ded61
upperdim <45048352+upperdim@users.noreply.github.com>
2022-08-13 21:21:03 +0300

    added lua implementation

---
+++ fp.lua
@@ -0,0 +1,17 @@
+function increment(n)
+	return n + 1
+end
+
+function my_objective(arr, f)
+	for i = 1, 5 do
+		arr[i] = f(arr[i])
+	end
+end
+
+function main()
+	local arr = {0, 1, 2 ,3 ,4}
+	my_objective(arr, increment)
+	for i = 1, 5 do io.write(arr[i] .. " ") end
+end
+
+main()
