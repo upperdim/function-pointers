355a896e174495d6374fa045fee6b7e6cb856136
upperdim <45048352+upperdim@users.noreply.github.com>
2022-08-13 21:10:29 +0300

    added javascript implementation

---
+++ fp.js
@@ -0,0 +1,16 @@
+function increment(n) {
+    return n + 1
+}
+
+function my_objective(arr, f) {
+    for (let i = 0; i < arr.length; ++i) {
+        arr[i] = f(arr[i])
+    }
+}
+
+function main() {
+    let arr = [0, 1, 2, 3 ,4]
+    my_objective(arr, increment)
+    console.log(arr)
+}
+main()
