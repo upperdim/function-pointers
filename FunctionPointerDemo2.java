d7e40b2d06eef985a9982975b754016bea63fc6c
Mert Aydın <mert.aydin@ykteknoloji.com.tr>
2023-03-13 01:26:02 +0300

    eliminated unnecessary object types

--- FunctionPointerDemo2.java
+++ FunctionPointerDemo2.java
@@ -1,22 +1,21 @@
+import java.util.Arrays;
 import java.util.function.Function;
 
 public class FunctionPointerDemo2 {
-    static Integer increment(Integer n) {
+    static int increment(int n) {
         return n + 1;
     }
 
-    static void myObjective(Integer[] arr, Function<Integer, Integer> f) {
+    static void myObjective(int[] arr, Function<Integer, Integer> f) {
         for (int i = 0; i < arr.length; ++i) {
             arr[i] = f.apply(arr[i]);
         }
     }
 
     public static void main(String[] args) {
-        Integer[] arr = {0, 1, 2, 3, 4};
-        myObjective(arr,  FunctionPointerDemo2::increment);
+        int[] arr = {0, 1, 2, 3, 4};
+        myObjective(arr, FunctionPointerDemo2::increment);
 
-        for (Integer elem : arr) {
-            System.out.print(elem + " ");
-        }
+        System.out.println(Arrays.toString(arr));
     }
 }
