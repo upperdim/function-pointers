1030cd34ce39f064b928cedbc7ae5d56bc4e4fb8
upperdim <45048352+upperdim@users.noreply.github.com>
2023-03-13 02:06:41 +0300

    removed unnecessary return

--- fp.py
+++ fp.py
@@ -5,10 +5,9 @@ def increment(n):
 def my_objective(arr, f):
     for i in range(len(arr)):
         arr[i] = f(arr[i])
-    return arr
 
 
 if __name__ == '__main__':
     arr = [0, 1, 2 , 3, 4]
-    arr = my_objective(arr, increment)
+    my_objective(arr, increment)
     print(arr)
