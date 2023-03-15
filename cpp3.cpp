a30a76cf9c73d57088d19b0bdcd3a2901e3d0f5f
Fevzi Yüksel <93267352+FevziYuksel@users.noreply.github.com>
2023-03-16 00:42:44 +0300

    Functional wrapper

---
+++ cpp3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+
+int increment(int n) {
+    return n + 1;
+}
+
+void print(const std::vector<int>& arr){
+    for(const int n : arr)
+        std::cout << n << "\n";
+    std::cout << std::endl;
+}
+
+void my_objective(std::vector<int>& arr, const std::function<int(int)>& f) {
+    for (int i = 0; i < arr.size(); ++i)
+        arr[i] = f(arr[i]);
+}
+
+int main() {
+    std::vector<int> arr = {0, 1, 2, 3, 4};
+
+    my_objective(arr, [](int n){ return n + 1;});
+
+    print(arr);
+
+    std::cin.get();
+    
+}
