dbaa021f1fec46beeac4d777f4baceac204e255b
Fevzi Yüksel <93267352+FevziYuksel@users.noreply.github.com>
2023-03-16 18:25:24 +0300

    Plain lambda

--- cpp2.cpp
+++ cpp2.cpp
@@ -9,7 +9,7 @@ void print(const std::vector<int>& vec){
 }
 
 void my_objective(std::vector<int>& vec, int (*f)(int)) {
-    for (int i = 0; i < arr.size(); ++i)
+    for (int i = 0; i < vec.size(); ++i)
         arr[i] = f(arr[i]);
 }
 
