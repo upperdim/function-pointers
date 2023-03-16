00bb450604cf46f58f797ca78e0e57a48bbe01cd
Fevzi Yüksel <93267352+FevziYuksel@users.noreply.github.com>
2023-03-16 18:27:32 +0300

    Function wrapper

--- cpp3.cpp
+++ cpp3.cpp
@@ -9,14 +9,14 @@ void print(const std::vector<int>& vec){
 }
 
 void my_objective(std::vector<int>& vec, const std::function<int(int)>& f) {
-    for (int i = 0; i < arr.size(); ++i)
-        arr[i] = f(arr[i]);
+    for (int i = 0; i < vec.size(); ++i)
+        vec[i] = f(vec[i]);
 }
 
 int main() {
     std::vector<int> vec = {0, 1, 2, 3, 4};
 
-    my_objective(vec, [](int n){ return n + 1;});
+    my_objective(vec, [&](int n){ return n + 1;});
 
     print(vec);
 
